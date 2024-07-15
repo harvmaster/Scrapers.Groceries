f54126a7f2a16857953f90446a63d9bbad69d52c
Harvmaster <hzuccon@gmail.com>
2024-07-15 23:43:21 +1000

    Removed legacy coles implementations

--- src/services/Coles/Coles.ts
+++
@@ -1,5 +0,0 @@
-import type { CategoryMapUrl } from "./types";
-
-export const CATEGORIES_SITEMAP: CategoryMapUrl = 'https://www.coles.com.au/sitemap/sitemap-categories.xml' as const
-
-export const API_TEMPLATE = 'https://www.coles.com.au/_next/data/{{version}}/en/browse/{{category}}.json?page={{page}}' as const
